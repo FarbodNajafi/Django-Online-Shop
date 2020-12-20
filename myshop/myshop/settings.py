51f62dbfd962d09936b6347a306788a90b36469d
Farbod Najafi <farbodnajafi@gmail.com>
2020-12-20 13:39:14 +0330

    Corrected a typo in installed apps

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
-    'coupons.apps.CouponConfig',
+    'coupons.apps.CouponsConfig',
 ]
 
 MIDDLEWARE = [
