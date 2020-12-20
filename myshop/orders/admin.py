1c0b2b92d331029e3c617ef2d00ff817f782310c
Farbod Najafi <farbodnajafi@gmail.com>
2020-12-20 12:21:28 +0330

    Added Invoice section in admin site to render pdf version of invoice

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -47,11 +47,19 @@ def order_detail(obj: Order):
     return mark_safe(f'<a href="{url}">View</a>')
 
 
+def order_pdf(obj: Order):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href={url}>PDF</a>')
+
+
+order_pdf.short_description = 'Invoice'
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated', order_detail]
+                    'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,12 +1,30 @@
+import weasyprint
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem, Order
 from .tasks import order_created
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(
+                                               settings.STATIC_ROOT / 'css/pdf.css'
+                                           )])
+    return response
 
 
 @staff_member_required
